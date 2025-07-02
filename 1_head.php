d1eab9e66ef9edf7367146b19c6c3e9bbfe77866
Kanan099 <kenan.rzayev028@gmail.com>
2025-07-02 14:50:15 +0400

    changed

--- 1_head.php
+++ 1_head.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Lesson</title>
 	<link rel="stylesheet" type="text/css" href="http://ders.loc/2/neww/3/1/css/reset.css">
 	<link rel="stylesheet" type="text/css" href="http://ders.loc/2/neww/3/1/css/style.css">
 </head>
